2b12e2bd542fac557d180986ffb369861745a573
EgorLomakin123 <egorlok26.10.2000@mail.ru>
2022-10-21 19:59:30 +0300

    се сделал

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5 import Qt
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton
 
 
 class Example(QWidget):
@@ -10,21 +10,48 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 300, 300)
+        self.setGeometry(300, 300, 500, 500)
         self.setWindowTitle('Координаты')
-
+        self.run_btn = QPushButton(self)
+        self.run_btn.setGeometry(100, 100, 100, 100)
+        self.run_btn.move(100, 100)
         self.coords = QLabel(self)
         self.coords.setText("Координаты: None, None")
         self.coords.move(30, 30)
+        self.last_cords = (0,0)
+        self.first = True
+
 
     def mouseMoveEvent(self, event):
-        self.coords.setText(f"Координаты: {event.x()}, {event.y()}")
+
+        x, y = event.x(), event.y()
+        print(x)
+        if self.first:
+            self.last_cords = (x, y)
+            self.first = False
+        else:
+            print()
+            xo = self.run_btn.pos().x()
+            yo = self.run_btn.pos().y()
+            if abs(self.last_cords[0] - xo) > abs(x - xo) and 1 < xo < 400:
+                xo += (-self.last_cords[0] + x) + 500
+            if abs(self.last_cords[1] - yo) > abs(y - yo) and 1 < yo < 400:
+                yo += (-self.last_cords[1] + y) + 500
+            print(xo, yo)
+            self.run_btn.move(xo % 500, yo % 500)
+
+            self.last_cords = (x, y)
+
 
     def keyPressEvent(self, event):
+        self.last_cords = (0,0)
         if int(event.modifiers()) == (Qt.AltModifier + Qt.ShiftModifier):
             if event.key() == Qt.Key_Q:
                 pass
-        # code
+
+    def mouseReleaseEvent(self, event):
+        self.first = True
+
 
 
 if __name__ == '__main__':

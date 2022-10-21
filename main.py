46bdf70b466408a052889b1a8649bd100727280f
arrgn <ilushenka11022006@yandex.ru>
2022-10-21 19:28:11 +0300

    fix some ???

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import sys
 
-<<<<<<< HEAD
 from itertools import combinations
 from PyQt5 import Qt
-=======
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter
->>>>>>> 245cddf911e0fdaa8f0e18114d49998eccde9abc
 from PyQt5.QtWidgets import QWidget, QApplication, QLabel
 from PyQt5.QtGui import QPainter, QColor
 
@@ -26,10 +23,9 @@ class Example(QWidget):
         self.coords.setText("Координаты: None, None")
         self.coords.move(30, 30)
 
-<<<<<<< HEAD
     def paintEvent(self, event) -> None:
         qp = QPainter()
-        qp.begin(self) #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
+        qp.begin(self)  # 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
         if len(self.dots) >= 2:
             for i in range(0, len(self.dots), 2):
                 x1, y1, x2, y2 = *self.dots[i], *self.dots[i + 1]
@@ -45,8 +41,6 @@ class Example(QWidget):
         self.dots.append((X, Y))
         self.repaint()
 
-
-=======
     def keyPressEvent(self, event):
         if int(event.modifiers()) == (Qt.AltModifier + Qt.ShiftModifier):
             if event.key == Qt.key_Q:
@@ -61,7 +55,6 @@ class Example(QWidget):
                 self.circleSize = 0
             self.update()
 
-
     def paintEvent(self, e):
         qp = QPainter()
         qp.begin(self)
@@ -71,7 +64,7 @@ class Example(QWidget):
     def drawCircle(self, qp):
         qp.setBrush(Qt.black)
         qp.drawEllipse(150 - self.circleSize // 2, 150 - self.circleSize // 2, self.circleSize, self.circleSize)
->>>>>>> 245cddf911e0fdaa8f0e18114d49998eccde9abc
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
